6fdff48e6321dccc031f80907b034917541211ad
Tinksdreamlife <tinksdreamlife@gmail.com>
2025-05-01 16:41:52 -0400

    removed all graveyard comments and cleaned up formatting

--- js/main.js
+++ js/main.js
@@ -18,50 +18,44 @@ let questionArray;
 
 let patronQues = [
   {
-    question: 'Patrons should tip dancers.', //Text of question 1'
+    question: 'Patrons should tip dancers.',
     answers: ['True', 'False'],
-    correctAnswer: 0, // index of 'Answer 1'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 0, 
     playerAnswer: null,
     gameResponse: 'Tipping shows respect for the entertainment a patron is enjoying. It is part of club etiquette.'
   },
   {
-    question: 'A patron must dress up to enter a club', //Text of question 2
+    question: 'A patron must dress up to enter a club', 
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 2'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null, 
     gameResponse: 'Proper hygeniene is encouraged. And dressing nicely is appreciated. But rarely is a suit and tie or formal attire required!'
   },
   {
-    question: 'You must be rich to visit a club', //Text of question 3
+    question: 'You must be rich to visit a club', 
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 3'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1, 
     playerAnswer: null,
     gameResponse: 'You do not have to be wealthy. However, this is not a free show! Therefore plan to spend money and enjoy the experience.'
   },
   {
-    question: 'A dancer WANTS to come home with you.', //Text of question 4
+    question: 'A dancer WANTS to come home with you.',
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 4'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null,
     gameResponse: 'Absolutely not, so do not ask. A dancer is an entertainer. Period. Enjoy the show, and then go home...alone.'
   },
   {
-    question: 'If sitting at the stage, you should tip every dancer.', //Text of question 4
+    question: 'If sitting at the stage, you should tip every dancer.', 
     answers: ['True', 'False'],
-    correctAnswer: 0, // index of 'Another Answer 5'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 0, 
     playerAnswer: null, 
     gameResponse: 'Yes! It is disrespectful to sit at the stage, and not show appreciation for the performance.'
   },
   {
-    question: 'Patrons may take pictures of dancers on the stage.', //Text of question 4
+    question: 'Patrons may take pictures of dancers on the stage.', 
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 6'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null, 
     gameResponse: 'Not only is it illegal in many states to take photos in a club, but it can be a safety concern for many performers who keep their lives private!'
   },
@@ -69,55 +63,48 @@ let patronQues = [
 
 let dancerQues = [
   {
-    question: 'Patrons are required to tip dancers.', //Text of question 1'
+    question: 'Patrons are required to tip dancers.',
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Answer 1'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null, 
     gameResponse: 'Sadly, they are not required to do so! But it is proper ettiquette for them to show monetary appreciation for our performances.'
   },
   {
-    question: 'Dancers must offer all club services to all club patrons', //Text of question 2
+    question: 'Dancers must offer all club services to all club patrons',
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 2'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null,
     gameResponse: 'Dancers may refuse to offer services to anyone for any reason.'
   },
   {
-    question: 'You must have a “great” body to be a dancer.', //Text of question 3
+    question: 'You must have a “great” body to be a dancer.',
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 3'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null, 
     gameResponse: 'There is no standard look for a performer. Patrons enjoy dancers of all shapes and sizes.'
   },
   {
-    question: 'There is not much room for professional growth as a dancer.', //Text of question 4
+    question: 'There is not much room for professional growth as a dancer.',
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 4'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null,
     gameResponse: 'Not only can dancers grow into other areas of the industry - such as content creation or specialty clubs - but skills such as marketing, performance, and sales, translate well to many other industries.'
   },
   {
-    question: 'Patrons will always spend more based on looks rather than a connection.', //Text of question 4
+    question: 'Patrons will always spend more based on looks rather than a connection.',
     answers: ['True', 'False'],
-    correctAnswer: 1, // index of 'Another Answer 5'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 1,
     playerAnswer: null,
     gameResponse: 'Many longtime patrons prefer to have a deeper connection with a performer - which translates to a better and more enjoyable experience for both the patron and the dancer.'
   },
   {
-    question: 'Patrons may be interested in plus size dancers.', //Text of question 4
+    question: 'Patrons may be interested in plus size dancers.',
     answers: ['True', 'False'],
-    correctAnswer: 0, // index of 'Another Answer 6'
-    //How do I add a "response" to give a piece of advice with the answer?
+    correctAnswer: 0,
     playerAnswer: null,
     gameResponse: 'Yes! Patrons love performers of all sizes, shapes, and backgrounds!'
   },
 ];
-
 
 let curQuestions = [];
 let curQuestionIdx = 0;
@@ -208,12 +195,11 @@ function showQuestion() {
 
 
 function selectAnswer(answerIdx) {
-  // const isCorrectAnswer = curQuestions[curQuestionIdx].correctAnswer === answerIdx;
 
   const curQuestion = curQuestions[curQuestionIdx];
   curQuestion.playerAnswer = answerIdx;
 
-  const chosenAnswer = curQuestion.answers[answerIdx]; //get the current answer
+  const chosenAnswer = curQuestion.answers[answerIdx]; 
   const isCorrectAnswer =curQuestion.correctAnswer === answerIdx;
 
   let feedback ='';
@@ -225,13 +211,7 @@ function selectAnswer(answerIdx) {
     feedback = `<p style = "color: red", "text-align: center">You chose "${chosenAnswer}, and that is INCORRECT!</p>`;
   }
 
-  // quizArea.innerHTML += `<p style = "color: purple", "text-align: center">You chose "${chosenAnswer}, and that is CORRECT!</p>`;
-  // } else {
-  //   quizArea.innerHTML += `<p style = "color: red", "text-align: center">You chose "${chosenAnswer}, and that is INCORRECT!</p>`;
-  // }
-
   const tip = `<p style="text-align: center; font-style: italic;">${curQuestion.gameResponse}</p>`;
-
   quizArea.innerHTML += feedback + tip;
 
   curQuestionIdx++;
